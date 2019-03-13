3f85682b99161e7f41994750ffb638ca2e3471ed
zhangmeng <569866591@qq.com>
2019-03-13 16:52:12 +0800

    更改Tomcat 密码

--- newcar-api/src/main/java/com/zchx/newcar/api/NewcarApiApplication.java
+++ newcar-api/src/main/java/com/zchx/newcar/api/NewcarApiApplication.java
@@ -1,11 +1,13 @@
 package com.zchx.newcar.api;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableDubboConfiguration
 public class NewcarApiApplication extends SpringBootServletInitializer {
 
 	@Override
